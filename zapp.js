d344f6379f12090f2b2a6575c2cb75fa50753de8
Zachary Helfenstein <zhelfinstein@megatron.cs.ucsb.edu>
2015-01-31 10:00:41 -0800

    make it work for a moment before we break it again

--- zapp.js
+++ zapp.js
@@ -1,7 +1,7 @@
 function go() {
 	$("#kills").css("font", "bold 30px sans-serif")
-                   .css("color", "green")
-                   .css("margin-left", $("body").width/2- $("#kills").width / 2);
+                   .css("color", "green");
+                   //.css("margin-left", $("body").width/2- $("#kills").width / 2);
         $("#deaths").css("font", "bold 30px sans-serif")
                     .css("color", "red");
 }
